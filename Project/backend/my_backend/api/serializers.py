d7c8d3d04f754b6b42d8e17e4bbd6aa8f4669463
Hwangzii <132841481+Hwangzii@users.noreply.github.com>
2024-12-12 09:34:54 +0700

    phi sửa cấu trúc

--- Project/backend/my_backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Account
-
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = ['id', 'username', 'password','position_id']  # Các trường bạn muốn trả về
--- Project/backend/my_backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LoginView, SignupView, AccountListView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('accounts/', AccountListView.as_view(), name='account-list'),
-]
